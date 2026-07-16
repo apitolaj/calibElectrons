f342d7716af4bc9278fe58ae7661cc47b231f4df
Andrew Pitolaj <andrewpitolaj@gmail.com>
2026-07-16 10:33:46 +0200

    added prototype master analysis script to increase compactness of the code

---
+++ BiCalib_template_indiv_sources/analysisScripts/ROOT/analysis_chainROOT.cpp
@@ -0,0 +1,90 @@
+//user must change this path to where their MiModule is located.
+#include "/sps/nemo/scratch/apitolaj/Modules/MiModule/include/MiEvent.h"
+#include "UTILS_PLACEHOLDER/functionUtils.h"
+#include "TLatex.h"
+#include "TVector3.h"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include "TSystemDirectory.h"
+#include "TSystemFile.h"
+#include "TList.h"
+
+//user must change this path to where their MiModule is located.
+R__LOAD_LIBRARY(/sps/nemo/scratch/apitolaj/Modules/MiModule/lib/libMiModule.so);
+
+void analysis_chainROOT_Source_SOURCE_PLACEHOLDER(int minEnergy, int maxEnergy)
+{
+
+    int envelopeCount = 0;
+    int noEnvelopeCount = 0;
+    int totalCount = 0;
+
+    MiVector3D* calibSourceVertexPos_Source_SOURCE_PLACEHOLDER = new MiVector3D();
+	
+	if(sourcePositions.count("Source_SOURCE_PLACEHOLDER"))
+	{
+		auto [Y, Z] = sourcePositions.at("Source_SOURCE_PLACEHOLDER");
+		calibSourceVertexPos_Source_SOURCE_PLACEHOLDER->setY(Y);
+		calibSourceVertexPos_Source_SOURCE_PLACEHOLDER->setZ(Z);
+	}
+
+	TChain chain("Event");
+	populateChain(chain, "BASE_PLACEHOLDER/../SOURCES/Source_SOURCE_PLACEHOLDER/DATA/ROOTFiles");
+
+	MiEvent *Eve = new MiEvent();
+	chain.SetBranchAddress("Eventdata", &Eve);
+	
+	TFile *outFileROOT = new TFile("BASE_PLACEHOLDER/DST_PLACEHOLDER/analysisROOT.root", "RECREATE");
+
+    treeData dataTree;
+    makeTrees(outFileROOT, dataTree);
+
+	for (int i = 0; i < chain.GetEntries(); i++)
+	{
+    
+        dataTree.eventNumber = i;
+        chain.GetEntry(i);
+
+        if(isEnergyinRange(Eve, minEnergy, maxEnergy) && isCalibElectron(Eve)) 
+        {
+            dataTree.zenith  = calculateZenith(Eve);
+            dataTree.azimuth = calculateAzimuth(Eve);
+            dataTree.DistOM  = calculateDistOM(Eve, calibSourceVertexPos_Source_SOURCE_PLACEHOLDER);
+            dataTree.DistTPP = calculateDistTPP(Eve, calibSourceVertexPos_Source_SOURCE_PLACEHOLDER);
+            dataTree.TPPy    = calculatePosTPPVector(Eve).getY();
+            dataTree.TPPz    = calculatePosTPPVector(Eve).getZ();
+
+            if (didInteractEnvelope(Eve))
+            {
+                dataTree.envelope->Fill();
+                envelopeCount++;
+            }
+
+            else
+            {
+                dataTree.noEnvelope->Fill();
+                noEnvelopeCount++;
+            }
+
+            totalCount++;
+	    }
+    }
+
+        delete calibSourceVertexPos_Source_SOURCE_PLACEHOLDER;
+
+        dataTree.envelope->Write("", TObject::kOverwrite);
+        dataTree.noEnvelope->Write("", TObject::kOverwrite);
+        dataTree.envelope->Delete();
+        dataTree.noEnvelope->Delete();
+        delete outFileROOT;
+           
+        std::ofstream outFileText("BASE_PLACEHOLDER/DST_PLACEHOLDER/angles_entryCounts_allEnergies_Source_SOURCE_PLACEHOLDER.txt");
+        outFileText << "Entry count (all energies)"       << std::endl;
+        outFileText << "Envelope: "    << envelopeCount   << std::endl;
+        outFileText << "No Envelope: " << noEnvelopeCount << std::endl;
+        outFileText << "Total: "       << totalCount      << std::endl;
+        outFileText.close();
+}
--- BiCalib_template_indiv_sources/analysisScripts/ROOT/functionUtils.h
+++ BiCalib_template_indiv_sources/analysisScripts/ROOT/functionUtils.h
@@ -20,7 +20,97 @@ float calculateDistTPP(MiEvent *Event, MiVector3D *calibSourceVertexPos);
 
 float calculateDistOM(MiEvent *Event, MiVector3D *calibrationSourceVertexPos);
 
+inline void makeTrees(TFile* outFile, treeData& trees); 
+
+struct treeData
+{
+
+	TTree* envelope;
+	TTree* noEnvelope;
+
+	double azimuth;
+	double zenith;
+	double DistOM;
+	double DistTPP;
+	double TPPy;
+	double TPPz;
+
+	int eventNumber;
+
+};
+
+inline const std::unordered_map<std::string, std::pair<double, double>> sourcePositions = {
+    {"Source_0_6", {-2089.0, 1272.0}},
+    {"Source_1_6", {-1255.0, 1280.0}},
+    {"Source_2_6", {-418.0,  1282.0}},
+    {"Source_3_6", { 417.0,  1279.0}},
+    {"Source_4_6", { 1253.5, 1284.0}},
+    {"Source_5_6", { 2088.0, 1278.0}},
+
+    {"Source_0_5", {-2088.0, 848.0}},
+    {"Source_1_5", {-1255.0, 853.0}},
+    {"Source_2_5", {-419.0,  857.0}},
+    {"Source_3_5", { 417.0,  856.0}},
+    {"Source_4_5", { 1254.0, 854.0}},
+    {"Source_5_5", { 2087.5, 850.0}},
+
+    {"Source_0_4", {-2088.0, 427.0}},
+    {"Source_1_4", {-1254.0, 433.0}},
+    {"Source_2_4", {-418.5,  436.0}},
+    {"Source_3_4", { 417.5,  431.0}},
+    {"Source_4_4", { 1254.0, 428.0}},
+    {"Source_5_4", { 2087.0, 422.0}},
+
+    {"Source_0_3", {-2088.0, 0.0}},
+    {"Source_1_3", {-1254.0, 0.0}},
+    {"Source_2_3", {-418.0,  0.0}},
+    {"Source_3_3", { 418.0,  0.0}},
+    {"Source_4_3", { 1254.0, 0.0}},
+    {"Source_5_3", { 2088.0, 0.0}},
+
+    {"Source_0_2", {-2088.0, -434.0}},
+    {"Source_1_2", {-1254.0, -424.0}},
+    {"Source_2_2", {-418.0,  -422.0}},
+    {"Source_3_2", { 418.5,  -418.0}},
+    {"Source_4_2", { 1254.0, -424.0}},
+    {"Source_5_2", { 2087.0, -426.0}},
+
+    {"Source_0_1", {-2088.0, -861.0}},
+    {"Source_1_1", {-1254.0, -842.0}},
+    {"Source_2_1", {-419.0,  -841.0}},
+    {"Source_3_1", { 419.5,  -848.0}},
+    {"Source_4_1", { 1254.5, -848.0}},
+    {"Source_5_1", { 2087.5, -852.0}},
+
+    {"Source_0_0", {-2087.0, -1281.0}},
+    {"Source_1_0", {-1252.5, -1276.0}},
+    {"Source_2_0", {-419.5,  -1279.0}},
+    {"Source_3_0", { 420.0,  -1270.0}},
+    {"Source_4_0", { 1255.0, -1275.0}},
+    {"Source_5_0", { 2087.0, -1277.0}},
+};
+
 //-----------------------------------------------------------------------
+
+inline void makeTrees(TFile* outFile, treeData& trees) 
+{
+
+    outFile->cd();
+
+    trees.envelope   = new TTree("envelope");
+    trees.noEnvelope = new TTree("noEnvelope");
+
+    for (TTree* t : {trees.envelope, trees.noEnvelope}) 
+	{
+        t->Branch("eventNum",   &trees.eventNumber,  "eventNumber/I");
+        t->Branch("azimuth" ,   &trees.azimuth    ,  "azimuth/D");
+        t->Branch("zenith"  ,   &trees.zenith     ,  "zenith/D");
+		t->Branch("DistOM"  ,   &trees.DistOM     ,  "DistOM/D");
+        t->Branch("DistTPP" ,   &trees.DistTPP    ,  "DistTPP/D");
+		t->Branch("TPPy"    ,   &trees.TPPy       ,  "TPPy/D");
+        t->Branch("TPPz"    ,   &trees.TPPz       ,  "TPPz/D");
+    }
+}
 
 float calculateDistOM(MiEvent *Event, MiVector3D *calibSourceVertexPos)
 {
@@ -73,6 +163,27 @@ float calculateDistTPP(MiEvent *Event, MiVector3D *calibSourceVertexPos)
 	
 }
 
+bool isEnergyinRange(MiEvent *Event, int minEnergy, int maxEnergy)
+{
+
+	double E_OM = 0.0;
+
+	std::vector<MiCDCaloHit>* caloHitVector = Event->getPTD()->getpart(0)->getcalohitv();
+
+	for(MiCDCaloHit& hit : *caloHitVector)
+	{
+		E_OM+=hit.getE();
+	}
+
+	if(minEnergy < E_OM && E_OM < maxEnergy)
+	{
+		return true;
+	}
+	
+	return false;
+}
+
+/*/
 bool isEnergyBetween600_800keV(MiEvent *Event)
 {
 
@@ -145,6 +256,33 @@ bool isCalibElectron_noEnvelope(MiEvent *Event)
         return (isCalibElectron && !didInteractEnvelope);
 }
 
+*/
+
+bool envelopeInteraction(MiEvent *Event)
+{
+	bool didInteractEnvelope= false;
+	MiSD *sd = Event->getSD();
+    
+	if (sd)
+        {
+        	std::vector<MiSDCaloHit> *ch = sd->getcalohitv();
+
+        	if (ch)
+        	{
+            		for (auto &hit : *ch)
+            		{            				
+            			if(hit.getGID()->gettype() == "1066")
+            			{
+            				didInteractEnvelope=true;
+            			}
+            			
+            		}
+        	}
+        }
+        
+        return didInteractEnvelope;
+}
+
 float calculateZenith(MiEvent *Event)
 {
 	TVector3 dir = Event->getPTD()->getpart(0)->getdirectionfromfoil();
@@ -203,60 +341,11 @@ void populateChain(TChain &ROOTChain, const char *dirPath)
 				
 				TString fullPath = TString(dirPath) + "/" + fname;
 				ROOTChain.Add(fullPath);
-		        }
+		    }
 		}
 	}
 }
 
-inline const std::unordered_map<std::string, std::pair<double, double>> sourcePositions = {
-    {"Source_0_6", {-2089.0, 1272.0}},
-    {"Source_1_6", {-1255.0, 1280.0}},
-    {"Source_2_6", {-418.0,  1282.0}},
-    {"Source_3_6", { 417.0,  1279.0}},
-    {"Source_4_6", { 1253.5, 1284.0}},
-    {"Source_5_6", { 2088.0, 1278.0}},
 
-    {"Source_0_5", {-2088.0, 848.0}},
-    {"Source_1_5", {-1255.0, 853.0}},
-    {"Source_2_5", {-419.0,  857.0}},
-    {"Source_3_5", { 417.0,  856.0}},
-    {"Source_4_5", { 1254.0, 854.0}},
-    {"Source_5_5", { 2087.5, 850.0}},
-
-    {"Source_0_4", {-2088.0, 427.0}},
-    {"Source_1_4", {-1254.0, 433.0}},
-    {"Source_2_4", {-418.5,  436.0}},
-    {"Source_3_4", { 417.5,  431.0}},
-    {"Source_4_4", { 1254.0, 428.0}},
-    {"Source_5_4", { 2087.0, 422.0}},
-
-    {"Source_0_3", {-2088.0, 0.0}},
-    {"Source_1_3", {-1254.0, 0.0}},
-    {"Source_2_3", {-418.0,  0.0}},
-    {"Source_3_3", { 418.0,  0.0}},
-    {"Source_4_3", { 1254.0, 0.0}},
-    {"Source_5_3", { 2088.0, 0.0}},
-
-    {"Source_0_2", {-2088.0, -434.0}},
-    {"Source_1_2", {-1254.0, -424.0}},
-    {"Source_2_2", {-418.0,  -422.0}},
-    {"Source_3_2", { 418.5,  -418.0}},
-    {"Source_4_2", { 1254.0, -424.0}},
-    {"Source_5_2", { 2087.0, -426.0}},
-
-    {"Source_0_1", {-2088.0, -861.0}},
-    {"Source_1_1", {-1254.0, -842.0}},
-    {"Source_2_1", {-419.0,  -841.0}},
-    {"Source_3_1", { 419.5,  -848.0}},
-    {"Source_4_1", { 1254.5, -848.0}},
-    {"Source_5_1", { 2087.5, -852.0}},
-
-    {"Source_0_0", {-2087.0, -1281.0}},
-    {"Source_1_0", {-1252.5, -1276.0}},
-    {"Source_2_0", {-419.5,  -1279.0}},
-    {"Source_3_0", { 420.0,  -1270.0}},
-    {"Source_4_0", { 1255.0, -1275.0}},
-    {"Source_5_0", { 2087.0, -1277.0}},
-};
 
 
